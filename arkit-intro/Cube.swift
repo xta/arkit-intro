3c8b9d1c4642f8c56e1b3b88dad4ca2b4bab2f0c
Rex Feng <rexfeng@gmail.com>
2017-09-17 00:14:06 -0700

    able to insert white cube based on tap gesture

---
+++ arkit-intro/Cube.swift
@@ -0,0 +1,41 @@
+//
+//  Cube.swift
+//  arkit-intro
+//
+//  Created by Rex on 9/17/17.
+//  Copyright © 2017 Edu. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+// based on https://github.com/markdaws/arkit-by-example
+
+class Cube: SCNNode {
+    init(_ position: SCNVector3, with material: SCNMaterial) {
+        super.init()
+        
+        let dimension: Float = 0.2
+        let cube = SCNBox(width: CGFloat(dimension), height: CGFloat(dimension), length: CGFloat(dimension), chamferRadius: 0)
+        cube.materials = [material]
+        let node = SCNNode(geometry: cube)
+        
+        // The physicsBody tells SceneKit this geometry should be manipulated by the physics engine
+        node.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        node.physicsBody?.mass = 2.0
+        node.position = position
+        
+        self.addChildNode(node)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    class func currentMaterial() -> SCNMaterial {
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor(white: 1.0, alpha: 1.0)
+        return material
+    }
+    
+}
--- arkit-intro/ViewController.swift
+++ arkit-intro/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var planes: [ARPlaneAnchor:Plane] = [:]
+    var cubes: [Cube] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,6 +28,8 @@ class ViewController: UIViewController {
         
         // Stop the screen from dimming while we are using the app
         UIApplication.shared.isIdleTimerDisabled = true
+        
+        setupRecognizers()
     }
     
     let standardConfiguration: ARWorldTrackingConfiguration = {
@@ -52,7 +57,41 @@ class ViewController: UIViewController {
         // Release any cached data, images, etc that aren't in use.
     }
     
-    // MARK: - Planes
+    // MARK: - Gesture(s)
+    
+    func setupRecognizers() {
+        // Single tap will insert a new piece of geometry into the scene
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(insertCubeFrom))
+        tapGestureRecognizer.numberOfTapsRequired = 1
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func insertCubeFrom(recognizer: UITapGestureRecognizer) {
+        // Take the screen space tap coordinates and pass them to the hitTest method on the ARSCNView instance
+        let tapPoint = recognizer.location(in: self.sceneView)
+        let result = self.sceneView.hitTest(tapPoint, types: ARHitTestResult.ResultType.existingPlaneUsingExtent)
+        
+        // If the intersection ray passes through any plane geometry they will be returned, with the planes
+        // ordered by distance from the camera
+        if result.count == 0 {
+            return
+        }
+        
+        // If there are multiple hits, just pick the closest plane
+        let hitResult = result.first
+        self.insertCube(hitResult: hitResult!)
+    }
+    
+    
+    func insertCube(hitResult: ARHitTestResult) {
+        // We insert the geometry slightly above the point the user tapped, so that it drops onto the plane
+        // using the physics engine
+        let insertionYOffset: Float = 0.5
+        let position = SCNVector3Make(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y.advanced(by: insertionYOffset), hitResult.worldTransform.columns.3.z)
+        
+        let cube = Cube.init(position, with: Cube.currentMaterial())
+        self.cubes.append(cube)
+        self.sceneView.scene.rootNode.addChildNode(cube)
+    }
 
-    var planes = [ARPlaneAnchor: Plane]()
 }
